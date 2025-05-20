e443512f42f93a38cbbdc4dee8f79d869d0d7fc2
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-05-20 05:45:05 +0100

    area operation

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -10,6 +10,7 @@ savings = input_income - monthly_expense
 print(f"Your monthly savings are ${savings}.")
 
 projected_savings = savings * 12 + (savings * 12 * 0.05)
+
 # i want to correct the floating point number to 2 decimal places
-print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")
 
