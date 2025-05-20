0ce169741c49b69f421406d44bf8967c26bae832
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-05-20 05:11:25 +0100

    simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000  # $1000
+rate = 0.05       # 5% annual interest rate
+time = 3          # 3 years
+
+simple_interest = principal * rate * time
+
+print(f"Simple Interest  is {simple_interest}")
