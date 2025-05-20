9ebf1acc0af3cd829e916df24c30fe4aab5a53f7
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-05-20 05:20:03 +0100

    hours to seconds"

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,4 +1,5 @@
-hours  = 2
+hours = 2
 
-
-print(f"{hours} hours is equal to {hours * 3600} seconds")
+#  - hours_to_seconds.py doesn't contain: ["seconds = hours * 3600"] - hours_to_seconds.py doesn't contain: ["seconds=hours*3600"] - hours_to_seconds.py doesn't contain: ["seconds = 3600 * hours"] - hours_to_seconds.py doesn't contain: ["seconds=3600*hours"]
+seconds = hours * 3600
+print(f"{hours} hours is equal to {seconds} seconds")
