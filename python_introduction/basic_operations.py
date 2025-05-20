6281ea4facf0b779e83afac3fc35f9b62f9dab99
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-05-20 05:02:10 +0100

    arthmetric operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,18 @@
+# Addition of 10 and 5 is 15
+# Subtraction of 10 and 5 is 5
+# Multiplication of 10 and 5 is 50
+
+number1 = 10
+number2 = 5
+
+# Addition
+addition = number1 + number2
+print(f"Addition of {number1} and {number2} is {addition}")
+
+# Subtraction
+subtraction = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+
+# Multiplication
+multiplication = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
