f817faeb9a468201189aed07fe60ddf73bd37c31
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-05-28 21:27:29 +0100

    feat: match case calculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+operation = str(input("Choose the operation (+, -, *, /).: "))
+
+if operation == "+":
+    print("The result is", num1 + num2);
+elif operation == "-":
+    print("The result is", num1 - num2);
+elif operation == "*":
+    print("The result is", num1 * num2);
+elif operation == "/":
+    if num2 == 0:
+        print("Cannot divide by zero.")
+    else:
+        print("The result is", num1 / num2);
