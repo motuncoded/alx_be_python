a8bdb1eeb7bc83a1a6eb693200e4c05987786cd3
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-05-29 18:17:22 +0100

    pattern_drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,1 +1,11 @@
-length = int(input("Enter the size of the pattern:."))
+
+length = int(input("Enter the size of the pattern:"))
+
+while length <= 0:
+    length = int(input("Please enter a positive integer: "))
+row = 0
+while row < length:
+    for col in range(length):
+        print("*", end="")
+    print()  
+    row += 1
