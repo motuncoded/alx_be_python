20b431efdf2887da57e64cce0a620f5e0157b43b
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-06-07 22:10:36 +0100

    arithmetric function created

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,22 @@
+def main():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    if operation == "add":
+        print("The result is", num1 + num2);
+    elif operation == "subtract":
+        print("The result is", num1 - num2);
+    elif operation == "multiply":
+        print("The result is", num1 * num2);
+    elif operation == "divide":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            print("The result is", num1 / num2);  
+        
+    else:
+        print("Invalid operation.")
+
+if __name__ == "__main__":
+    main()
