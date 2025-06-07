95990b45fe32336043fc6467f9469d478de89b1b
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-06-07 22:18:43 +0100

    arithmetric function created

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation():
+def perform_operation(num1, num2, operation):
     print("Arithmetic Operations")
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
