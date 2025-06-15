9c5dc35c1175e20cd0592043ff8531e12d450541
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-06-15 16:36:32 +0100

    task o

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,21 +1,25 @@
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.__account_balance = initial_balance
-
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            return f"Deposited: ${amount:.1f}"
-
-    def withdraw(self, amount):
-        if amount <= self.__account_balance:
-             self.__account_balance -= amount
-             print(f"Withdrew: ${amount:.1f}")
-             return True
+    def __init__(self, account_balance):
+        self.__account_balance = account_balance
+        
+        
+    def get_account_balance(self):
+        return self.__account_balance
+    def set_account_balance(self, account_balance):
+        if account_balance > 0:
+            self.__account_balance = account_balance
         else:
-            print("Insufficient funds.")
+            print("Your account balance is zero!")
+    
+    def deposit(self, amount):
+        self.__account_balance += amount
+    def withdraw(self,amount):
+        if amount <= self.__account_balance:
+          self.__account_balance   -= amount
+          return True
+        else:
             return False
     
-
     def display_balance(self):
-        return f"Current Balance: ${self.__account_balance:.2f}"
+        
+         print(f"Current Balance: ${self.__account_balance:.2f}")
